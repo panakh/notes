5370b7464d7ad785f38214e43d2c3c286172ce2a
Hash Panak <hashinp@gmail.com>
2019-09-16 12:09:29 +0100

    Updates

---
+++ precache-manifest.fac85b20706a027853776f42d4555470.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d5b8667f0b785c171dbfc5d11452574",
+    "url": "/notes/index.html"
+  },
+  {
+    "revision": "3052df2063a8cfa05b1a",
+    "url": "/notes/static/css/main.9cf2006a.chunk.css"
+  },
+  {
+    "revision": "0b63c48f12835f075625",
+    "url": "/notes/static/js/2.7ef67e07.chunk.js"
+  },
+  {
+    "revision": "3052df2063a8cfa05b1a",
+    "url": "/notes/static/js/main.703b3adb.chunk.js"
+  },
+  {
+    "revision": "1d0704df65d0b82930a0",
+    "url": "/notes/static/js/runtime~main.00bd8d49.js"
+  }
+]);
